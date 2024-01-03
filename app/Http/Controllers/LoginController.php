5c8680a17db8e4ffac7b1b3da7d9903f88b5b2ad
YORU-17\berli <berlianrusmana23@gmail.com>
2024-01-03 14:31:17 +0700

    init

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'    => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+
+            $request->session()->regenerate();
+            $user = Auth::user();
+            if ($user->position == "Admin" && $user->authorization_level == 2) {
+                return redirect()->route('dashboardAdmin');
+            }
+
+            if ($user->position == "Staff Human Resource" && $user->authorization_level == 1) {
+                return redirect()->route('dashboardHR');
+            }
+
+            if (($user->position == "Staff Inventory" && $user->authorization_level == 1) || ($user->position == "Supplier" && $user->authorization_level == 1)) {
+                return redirect()->route('dashboardInven');
+            }
+
+            if ($user->position == "Staff Sales" && $user->authorization_level == 1) {
+                return redirect()->route('dashboardSales');
+            }
+        }
+        return back()->with('loginError', 'Login Failed!');
+    }
+
+    // protected function authenticated(Request $request, $user)
+    // {
+
+    //     if ($user->position == "Staff Human Resource") {
+    //         return redirect()->intended('dashboard');
+    //     }
+
+    //     if ($user->position == "Staff Inventory") {
+    //         return redirect()->route('dashboardInven');
+    //     }
+
+    //     return redirect('/home');
+    // }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    public function callCenter()
+    {
+        return view('auth.CC');
+    }
+}
